a8ae422ac49e1cb9a6b74d952effe2bfd1560a00
gregorygregio <gregorygregio@hotmail.com>
2018-01-21 20:45:00 -0200

    adicionando jquery e bootstrap

--- js/index.js
+++ js/index.js
@@ -3,6 +3,4 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
 	console.log("device ready")
-  document.getElementById("titulo").value = "javascript funcionando";
-  alert("device ready")
 }
