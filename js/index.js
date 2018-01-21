614ce3b89a183d6de935c8fccfb0e04884704c5a
gregorygregio <gregorygregio@hotmail.com>
2018-01-21 21:03:14 -0200

    testando chamada ajax

--- js/index.js
+++ js/index.js
@@ -2,5 +2,31 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 
 function onDeviceReady() {
-	console.log("device ready")
+	console.log("device ready");
+
+	jQuery("#testeApi").click(function(){
+		var root = 'https://jsonplaceholder.typicode.com';
+
+		jQuery.ajax({
+		  url: root + '/posts/1',
+		  type: 'GET',
+			dataType: "json",
+      crossDomain: true,
+			success: function(data){
+				console.log(data);
+				jQuery("#testeSpan").text(data.body)
+			}
+		})
+
+
+	})
 }
+
+// var root = 'https://jsonplaceholder.typicode.com';
+//
+// $.ajax({
+//   url: root + '/posts/1',
+//   method: 'GET'
+// }).then(function(data) {
+//   console.log(data);
+// });
