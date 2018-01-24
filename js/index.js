1a4bcbbf1132c8486c6638c2174614f6fef6028d
gregorygregio <gregorygregio@hotmail.com>
2018-01-23 23:05:40 -0200

    navegacao de noticias com btns proximo e anterior

--- js/index.js
+++ js/index.js
@@ -1,35 +1,60 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+var criarObjetoPaginacao = function(){
+	var root = 'https://jsonplaceholder.typicode.com';
+
+	var _getPageData = (id) => {
+		jQuery.ajax({
+			url: root + '/posts/'+id,
+			type: 'GET',
+			dataType   : 'json',
+			success: function(data){
+				jQuery("#body").text(data.body)
+				jQuery("#title").text(data.title)
+			},
+			error:function (err) {
+
+				alert("Ocorreu um erro");
+				jQuery("#body").text(JSON.stringify(err))
+			}
+		})
+	}
+
+	var _proximo = function(){
+		_getPageData(this.page)
+		this.page++;
+	}
+
+	var _anterior = function(){
+		if(this.page > 1){
+			_getPageData(this.page)
+			this.page--;
+		}
+	}
+	return {
+		page:1,
+		proximo:_proximo,
+		anterior:_anterior
+	}
+}
+
 
 function onDeviceReady() {
 	console.log("device ready");
 
-	jQuery("#testeApi").click(function(){
-		var root = 'https://jsonplaceholder.typicode.com';
+	var objetoPaginacao = criarObjetoPaginacao();
 
-		jQuery.ajax({
-		  url: root + '/posts/1',
-		  type: 'GET',
-			dataType   : 'json',
-			success: function(data){
-				console.log(data);
-				jQuery("#testeSpan").text(data.body)
-			},
-			error:function (err) {
-				alert("Ocorreu um erro");
-				jQuery("#testeSpan").text(JSON.stringify(err))
-			}
-		})
-
-
+	jQuery("#btnProximo").click(function(){
+		objetoPaginacao.proximo();
 	})
-}
 
-// var root = 'https://jsonplaceholder.typicode.com';
-//
-// $.ajax({
-//   url: root + '/posts/1',
-//   method: 'GET'
-// }).then(function(data) {
-//   console.log(data);
-// });
+	jQuery("#btnAnterior").click(function(){
+		objetoPaginacao.anterior();
+	})
+
+
+
+
+
+	objetoPaginacao.proximo();
+}
