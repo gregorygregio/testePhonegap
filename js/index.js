ed38a8d7c8a9b4e82dba93e3564274fb76db1c8c
gregorygregio <gregorygregio@hotmail.com>
2018-01-25 22:00:04 -0200

    inserindo barra de busca

--- js/index.js
+++ js/index.js
@@ -4,19 +4,51 @@ var api = 'https://jsonplaceholder.typicode.com';
 
 var template = jQuery("#userPillTemplate").clone();
 
-function getListOf(thing){
+var userClick = function(){
+	var id = jQuery(this).find(".userId").text();
+	alert(id)
+}
+
+var userPillList = [];
+
+function getListOf(thing, _success){
 	jQuery.ajax({
 	  url: api + '/'+thing,
-	  method: 'GET'
-	}).then(function(data) {
-	  data.forEach(function(user){
-			var pill = template.html().replace("{{:userId}}", user.id).replace("{{:userName}}", user.name);
-			jQuery("#listOfUsers").append(pill);
-		})
+	  method: 'GET',
+		success: _success
 	});
 }
 
 
+String.prototype.has = function(anotherString){
+		return ( this.toLowerCase().indexOf(anotherString.toLowerCase()) > -1 );
+}
+
+Number.prototype.has = function(anotherString){
+		return ( this.toString().indexOf(anotherString) > -1 );
+}
+
 function onDeviceReady() {
-	getListOf("users")
+	getListOf("users", function(data) {
+	  	data.forEach(function(user){
+				var pill = jQuery(template.html().replace("{{:userId}}", user.id).replace("{{:userName}}", user.name));
+				pill.click(userClick);
+				userPillList.push( { id: user.id, name: user.name, element: pill } );
+				jQuery("#listOfUsers").append(pill);
+			})
+	});
+
+
+	jQuery("#searchBar").keyup( function(){
+			var $this = jQuery(this);
+			var valueToSearchFor = $this.val();
+
+			userPillList.forEach( function(userPill){
+					if( userPill.name.has(valueToSearchFor) || userPill.id.has(valueToSearchFor))
+						userPill.element.show();
+					else
+						userPill.element.hide();
+			})
+	})
+
 }
